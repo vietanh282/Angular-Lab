0b7ca48ab1c7be781c91d21b932354c76ccd93de
Vietanh <ngovietanh503@gmail.com>
2024-01-18 20:15:55 +0700

    Ui: login end register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,16 @@ import { ProductsComponent } from './pages/admin/products/products.component';
 import { AdminComponent } from './layouts/admin/admin.component';
 import { CreateComponent } from './pages/admin/create/create.component';
 import { EditComponent } from './pages/admin/edit/edit.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 export const routes: Routes = [
   // route '/' = page Home
   // path, component
   { path: '', component: HomeComponent },
   // { path: 'admin', component: ProductsComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+
   {
     path: 'admin',
     component: AdminComponent,
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
